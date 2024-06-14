e3abb5259085935cdcca03bf1769a95f789e83c5
hoangminhnam <namhmhe176539@fpt.edu.vn>
2024-06-14 21:17:23 +0700

    change form submit feedback

---
+++ PetStoreProject/PetStoreProject/Controllers/FeedbackController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetStoreProject.Models;
+using PetStoreProject.ViewModels;
+
+namespace PetStoreProject.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly PetStoreDBContext _context;
+
+        public FeedbackController(PetStoreDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Submit()
+        {
+            int pid = Int32.Parse(Request.Form["pid"]);
+			Feedback feedback = new Feedback();
+            feedback.Name = Request.Form["FullName"];
+            feedback.Phone = Request.Form["PhoneNumber"];
+            feedback.Email = Request.Form["Email"];
+            feedback.Rating = Int32.Parse(Request.Form["rating"]);
+            feedback.Content = Request.Form["Content"];
+            feedback.DateCreated = DateTime.Now;
+            _context.Feedbacks.Add(feedback);
+            _context.SaveChanges();
+			return RedirectToAction("Detail", "Product", new { productId = Request.Form["pid"] });
+		}
+
+        [HttpPost]
+        public JsonResult CheckPhoneNumber(string phoneNumber)
+        {
+            bool phoneNumberExistsOrder = _context.Orders.Any(o => o.Phone == phoneNumber);
+
+            if (!phoneNumberExistsOrder)
+            {
+                return Json(new { status = "error", message = "Bạn không thể comment vì bạn chưa mua hàng." });
+            }
+
+            return Json(new { status = "success"});
+        }
+    }
+}

7ac461c59398b017343a54b47760edc4d1a5c1b5
hoangminhnam <namhmhe176539@fpt.edu.vn>
2024-07-23 02:16:23 +0700

    update pagination for screen listfeedback employee

--- PetStoreProject/PetStoreProject/Areas/Employee/Controllers/FeedbackController.cs
+++ PetStoreProject/PetStoreProject/Areas/Employee/Controllers/FeedbackController.cs
@@ -4,6 +4,8 @@ using PetStoreProject.Helpers;
 using PetStoreProject.Models;
 using PetStoreProject.Repositories.FeedBack;
 using PetStoreProject.ViewModels;
+using System.Collections.Immutable;
+using X.PagedList;
 
 namespace PetStoreProject.Areas.Employee.Controllers
 {
@@ -17,93 +19,92 @@ namespace PetStoreProject.Areas.Employee.Controllers
 			_feedback = feedbackRepository;
 			_dbContext = petStoreDBContext;
 		}
-
-
-        [HttpPost]
-        public IActionResult LoadMoreFeedback(int page, int status, DateTime from, DateTime to)
+        public IActionResult List(int? status, DateTime from, DateTime to, int? page, int? pageSize)
         {
-            var list = _feedback.GetListFeedBack(page).Where(x => x.CreatedDate > from && x.CreatedDate < to);
-            if (status == 1)
+            var list = _feedback.GetListFeedBack();
+            if(status != null)
             {
-                list = list.Where(x => x.Status == false);
-            }
-            else if (status == 2)
-            {
-                list = list.Where(x => x.Status == true);
-            }
-            return new JsonResult(new
-            {
-                listResult = list
-            });
-        }
-
-        [HttpPost]
-        public IActionResult List(int status, DateTime from, DateTime to)
-        {
-            var list = _feedback.GetListFeedBack(1).Where(x => x.CreatedDate > from && x.CreatedDate < to);
-            if (status == 1)
-            {
-                list = list.Where(x => x.Status == false);
-            }
-            else if (status == 2)
-            {
-                list = list.Where(x => x.Status == true);
+                if (status == 1)
+                {
+                    list = list.Where(x => x.Status == false).ToList();
+                }
+                else if (status == 2)
+                {
+                    list = list.Where(x => x.Status == true).ToList();
+                }
             }
 
+            if(from == DateTime.MinValue && to == DateTime.MinValue)
+            {
+                DateTime today = DateTime.Today;
+                DateTime firstDayOfWeek = DateTimeHelpers.GetFirstDayOfWeek(today);
+                DateTime lastDayOfWeek = DateTimeHelpers.GetLastDayOfWeek(today);
+                list = _feedback.GetListFeedBack().Where(x => x.CreatedDate > firstDayOfWeek && x.CreatedDate < lastDayOfWeek).ToList();
+                ViewBag.FirstDayOfWeek = firstDayOfWeek.ToString("yyyy-MM-dd");
+                ViewBag.LastDayOfWeek = lastDayOfWeek.ToString("yyyy-MM-dd");
+            }
+            else
+            {
+                ViewBag.FirstDayOfWeek = from.ToString("yyyy-MM-dd");
+                ViewBag.LastDayOfWeek = to.ToString("yyyy-MM-dd");
+                list = list.Where(x => x.CreatedDate > from && x.CreatedDate < to).ToList();
+            }
+
+            
             ViewBag.Status = status;
-            ViewBag.FirstDayOfWeek = from.ToString("yyyy-MM-dd");
-            ViewBag.LastDayOfWeek = to.ToString("yyyy-MM-dd");
             ViewBag.Length = list.Count();
-            return View(list);
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 12;
+            }
+            return View(list.ToPagedList((int)page, (int)pageSize));
         }
-
-
-        public IActionResult List()
-		{
-            DateTime today = DateTime.Today;
-            DateTime firstDayOfWeek = DateTimeHelpers.GetFirstDayOfWeek(today);
-            DateTime lastDayOfWeek = DateTimeHelpers.GetLastDayOfWeek(today);
-            var list = _feedback.GetListFeedBack(1).Where(x => x.CreatedDate > firstDayOfWeek && x.CreatedDate < lastDayOfWeek);
-            ViewBag.FirstDayOfWeek = firstDayOfWeek.ToString("yyyy-MM-dd");
-            ViewBag.LastDayOfWeek = lastDayOfWeek.ToString("yyyy-MM-dd");
-            ViewBag.Status = 0;
-            ViewBag.Length = list.Count();
-            return View(list);
-		}
 
         [HttpPost]
         public IActionResult Submit()
 		{
-            int fbId = Int32.Parse(Request.Form["FeedBackId"]);
+            try
+            {
+                int fbId = Int32.Parse(Request.Form["FeedBackId"]);
 
-            var feedBack = _dbContext.Feedbacks.SingleOrDefault(fb => fb.FeedbackId == fbId);
+                var feedBack = _dbContext.Feedbacks.SingleOrDefault(fb => fb.FeedbackId == fbId);
 
-            if (feedBack != null)
-                feedBack.Status = true;
-            var email = HttpContext.Session.GetString("userEmail");
-            // check have employee responed
-            if (_dbContext.ResponseFeedbacks.Any(x => x.FeedbackId == fbId))
-			{
-				var respFb = _dbContext.ResponseFeedbacks.Where(x => x.FeedbackId ==fbId).FirstOrDefault();
-				if(respFb != null)
-				{
-					respFb.EmployeeId = _dbContext.Employees.Where(e => e.Email == email).Select(e => e.EmployeeId).FirstOrDefault(); 
-					respFb.Content = Request.Form["response"];
-					respFb.DateCreated = DateTime.Now;
-					_dbContext.SaveChanges();
+                if (feedBack != null)
+                    feedBack.Status = true;
+                var email = HttpContext.Session.GetString("userEmail");
+                // check have employee responed
+                if (_dbContext.ResponseFeedbacks.Any(x => x.FeedbackId == fbId))
+                {
+                    var respFb = _dbContext.ResponseFeedbacks.Where(x => x.FeedbackId == fbId).FirstOrDefault();
+                    if (respFb != null)
+                    {
+                        respFb.EmployeeId = _dbContext.Employees.Where(e => e.Email == email).Select(e => e.EmployeeId).FirstOrDefault();
+                        respFb.Content = Request.Form["response"];
+                        respFb.DateCreated = DateTime.Now;
+                        _dbContext.SaveChanges();
+                    }
                 }
-			}
-			else
-			{
-                ResponseFeedback responseFeedback = new ResponseFeedback();
-                responseFeedback.FeedbackId = fbId;
-                responseFeedback.Content = Request.Form["response"];
-                responseFeedback.DateCreated = DateTime.Now;
-                responseFeedback.EmployeeId = _dbContext.Employees.Where(e => e.Email == email).Select(e => e.EmployeeId).FirstOrDefault();
-                _dbContext.ResponseFeedbacks.Add(responseFeedback);
-                _dbContext.SaveChanges();
+                else
+                {
+                    ResponseFeedback responseFeedback = new ResponseFeedback();
+                    responseFeedback.FeedbackId = fbId;
+                    responseFeedback.Content = Request.Form["response"];
+                    responseFeedback.DateCreated = DateTime.Now;
+                    responseFeedback.EmployeeId = _dbContext.Employees.Where(e => e.Email == email).Select(e => e.EmployeeId).FirstOrDefault();
+                    _dbContext.ResponseFeedbacks.Add(responseFeedback);
+                    _dbContext.SaveChanges();
+                }
+
             }
-            return RedirectToAction("List");
+            catch (Exception ex)
+            {
+                return BadRequest("Nội dung phản hồi không hợp lệ.");
+            }
+            return Ok("Phản hồi đã được gửi thành công.");
         }
     }
 }
